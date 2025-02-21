426a18bcebe61bff14a0cf20edef5ac382e57fb1
DanielGit <daniel@git.com>
2025-02-21 12:13:50 +0100

    Cambios en consulta

--- odoo/addons/dmr_veterinario/models/Consulta.py
+++ odoo/addons/dmr_veterinario/models/Consulta.py
@@ -5,16 +5,17 @@ class Consulta(models.Model):
     _name = 'dmr_veterinario.consulta'
     _description = 'Consulta'
 
+    name = fields.Char('Nombre')
     motivo = fields.Char('Motivo')
     fecha = fields.Date('Fecha')
-    diagnostico = fields.Char('Diagnostico')
-    tratamiento = fields.Char('Tratamiento')
-    precio = fields.Float('Precio')
     estado = fields.Selection([
         ('0', 'Pendiente'),
         ('1', 'Realizada'),
         ('2', 'Cancelada'),
     ], string='Estado')
+    diagnostico = fields.Char('Diagnostico')
+    tratamiento = fields.Char('Tratamiento')
+    precio = fields.Float('Precio')
     paciente_id = fields.Many2one('dmr_veterinario.paciente', string='Paciente')
 
 
@@ -25,4 +26,21 @@ class Consulta(models.Model):
             record.propietario_id = record.paciente_id.propietario_id if record.paciente_id else False
 
     propietario_id = fields.Many2one('dmr_veterinario.propietario', string='Propietario', compute="_compute_propietario", store=True, readonly=True)
+
+    # @api.depends('estado')
+    # def _compute_readonly_fields(self):
+    #     for record in self:
+    #         readonly = record.estado != '1'
+    #         record.diagnostico = False
+    #         record.tratamiento = False
+    #         record.precio = False
+
+
+#ESTO NO FUNCIONA TODAVIA
+    @api.constrains('diagnostico', 'tratamiento', 'precio')
+    def _check_fields(self):
+        for record in self:
+            if record.estado != '1' and (record.diagnostico or record.tratamiento or record.precio):
+                raise ValidationError("Los campos Diagnóstico, Tratamiento y Precio solo pueden ser llenados si el estado es 'Realizada'.")
+
     
